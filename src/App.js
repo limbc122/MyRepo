175cfcc1d0e072cfb2b756ab326a7f2bcc8d8408
User-23 <koscom23@gmail.com>
2018-07-03 15:56:04 +0900

    강의목표 컴포넌트 생성 실습
    넘흐넘흐 적응안됨. ㅜ_ㅜ

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const goals = [
+  "1. React 개발에 필요한 환경을 구축한다.",
+  "2. 새로운 자바스크립트 문법을 익힌다.",
+  "3. 새로운 자바스크립트 문법을 익힌다."
+];
+
+const LectureGoalList = props => {
+  return (
+    <div>
+      <div>{props.title}</div>
+      <ul>{props.items.map((item, idx) => <li key={idx}>{item}</li>)}</ul>
+    </div>
+  );
+};
 
 class App extends Component {
   render() {
@@ -13,6 +28,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <LectureGoalList items={goals} title={"강의 목표!"} />
       </div>
     );
   }
