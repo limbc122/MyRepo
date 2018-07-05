76e1f7ad4a53e2cc8fc051a9898a4ffcc8b8d79b
User-23 <koscom23@gmail.com>
2018-07-05 16:46:34 +0900

    마지막 저장 React 뱌뱌~

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import moment from "moment";
 import Todos from "./components/Todos";
 import Timer from "./components/Timer";
 import Header from "./components/Header";
-import logo from "./logo.svg";
+import AsyncState from "./components/AsyncState";
 import "./App.css";
-import "antd/dist/antd.css";
+import { Icon } from "antd";
+// import axios from "axios";
 
 const goals = [
   { title: "1. React 개발에 필요한 환경을 구축한다.", completed: true },
@@ -13,44 +13,28 @@ const goals = [
   { title: "3. 새로운 자바스크립트 문법을 익힌다.", completed: false }
 ];
 
-const TIME_EXPIRED = "2018-07-05T16:10:00+09:00";
-
 class App extends Component {
   state = {
     isExpired: false,
     dateStr: "2018-07-05T16:10:00+09:00"
   };
 
-  constructor(props) {
-    console.log("constructor");
-    super(props);
-  }
-
-  handleComplete = () => {
-    this.setState({ isExpired: true });
-    console.log("Timer call");
-  };
-
-  handleClick = () => {};
+  // componentDidMount() {
+  //   axios.get("https://tvjan-tvmaze-v1.p.mashape.com/schedule").then(({ data }) => {
+  //     this.setState({ data });
+  //   });
+  // }
 
   render() {
-    //const isExpired = moment(TIME_EXPIRED) < moment();
-
     const { isExpired, dateStr } = this.state;
 
     return (
       <div className="App">
         <Header />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
         <Todos items={goals} title={"강의 목표!"} />
         {!isExpired && <Timer key={dateStr} expireDate={dateStr} onComplete={this.handleComplete} />}
       </div>
     );
-  }
-  componentWillUnmount() {
-    console.log("unmount");
   }
 }
 
--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,25 +1,61 @@
-import React from "react";
-import { Checkbox } from "antd";
+import React, { Component } from "react";
+import { Checkbox, Input, Icon } from "antd";
+// import "./Todos.less";
 
-const ListItem = ({ title, completed }) => {
-  return (
-    <li>
-      <Checkbox defaultChecked={completed}>{title}</Checkbox>
-    </li>
-  );
-};
+class Todos extends Component {
+  constructor(props) {
+    super(props);
 
-const Todos = props => {
-  return (
-    <div className="Todos">
-      <div>{props.title}</div>
-      <ul>
-        {props.items.map((item, idx) => {
-          return <ListItem key={idx} {...item} />;
-        })}
-      </ul>
-    </div>
-  );
-};
+    this.state = {
+      items: [...props.items]
+    };
+  }
+
+  handleTrashClick = (e, idx, item) => {
+    let { items } = this.state;
+    items = items.splice(idx, 1);
+
+    this.setState({
+      items: [...items]
+    });
+  };
+
+  handlePressEnter = e => {
+    console.log(e.target.value);
+
+    const { items } = this.state;
+    items.push({
+      title: e.target.value,
+      completed: false
+    });
+
+    this.setState({
+      items: [...items]
+    });
+  };
+
+  render() {
+    const { title } = this.props;
+    const { items } = this.state;
+
+    return (
+      <div className="Todos">
+        <div>{title}</div>
+        <div>
+          <strike>추가</strike> : <Input type="textbox" name="txtBox" onPressEnter={this.handlePressEnter} />
+        </div>
+        <ul>
+          {items.map((item, idx) => (
+            <li>
+              <Icon className="icon" type="delete" onClick={e => this.handleTrashClick(e, idx, item)} />
+              <Checkbox className={item.completed ? "W-strike" : "W-Nonstrike"} defaultChecked={item.completed} />
+              {item.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
 
 export default Todos;
